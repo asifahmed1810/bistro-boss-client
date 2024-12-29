fdaf16caedba1dc840e4e11ff98daebd575e3bf4
asifahmed1810 <asifahmed18oct@gmail.com>
2024-12-29 23:17:55 +0600

    added hook funtionality

--- src/pages/Home/PopularSection/PopularSection.jsx
+++ src/pages/Home/PopularSection/PopularSection.jsx
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import SectionTitle from '../../../components/Sectiontitle/SectionTitle';
 import MenuItem from '../../../shared/MenuItem';
+import useMenu from '../../../hooks/useMenu';
 
 const PopularSection = () => {
-    const [menu,setMenu]=useState([]);
-    useEffect(()=>{
-        fetch('menu.json')
-        .then(res=>res.json())
-        .then(data=>{
-            const popularItems=data.filter(item=>item.category ==='popular')
-            setMenu(popularItems)})
-    },[])
+    const [menu]=useMenu();
+    const popular=menu.filter(item=>item.category ==='popular');
+    // const [menu,setMenu]=useState([]);
+    // useEffect(()=>{
+    //     fetch('menu.json')
+    //     .then(res=>res.json())
+    //     .then(data=>{
+    //         const popularItems=data.filter(item=>item.category ==='popular')
+    //         setMenu(popularItems)})
+    // },[])
     return (
         <section className='mb-12'>
             <SectionTitle
@@ -18,7 +21,7 @@ const PopularSection = () => {
             subheading={'Popular Items'}></SectionTitle>
             <div className='grid md:grid-cols-2 gap-10'>
                 {
-                    menu.map(item=><MenuItem key={item._id} item={item}></MenuItem>)
+                    popular.map(item=><MenuItem key={item._id} item={item}></MenuItem>)
                 }
             </div>
         </section>
