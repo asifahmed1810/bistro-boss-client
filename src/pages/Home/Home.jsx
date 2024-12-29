f8b6136ddad9a03d1ec9fc83bb484789883d1474
asifahmed1810 <asifahmed18oct@gmail.com>
2024-12-29 18:43:55 +0600

    Added popular menu  section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from './Banner/Banner';
 import Category from './Category/Category';
+import PopularSection from './PopularSection/PopularSection';
 
 const Home = () => {
     return (
         <div>
            <Banner></Banner>
            <Category></Category>
+           <PopularSection></PopularSection>
         </div>
     );
 };
