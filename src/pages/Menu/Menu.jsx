e7af10f95af81efbba0db3374dc850bd38b6c6bc
asifahmed1810 <asifahmed18oct@gmail.com>
2024-12-29 22:34:50 +0600

    added cover section

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import {Helmet } from 'react-helmet-async';
+import Cover from '../../shared/Cover';
+import menuImg from '../../assets/menu/banner3.jpg'
+import PopularSection from '../Home/PopularSection/PopularSection';
 
 const Menu = () => {
     return (
@@ -7,6 +10,14 @@ const Menu = () => {
             <Helmet>
                 <title>Bistro | Menu</title>
             </Helmet>
+            <Cover img={menuImg} title={'our menu'}></Cover>
+            <PopularSection></PopularSection>
+            <Cover img={menuImg} title={'our menu'}></Cover>
+            <PopularSection></PopularSection>
+            <Cover img={menuImg} title={'our menu'}></Cover>
+            <PopularSection></PopularSection>
+            <Cover img={menuImg} title={'our menu'}></Cover>
+            <PopularSection></PopularSection>
         </div>
     );
 };
